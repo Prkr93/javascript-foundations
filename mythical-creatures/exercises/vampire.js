48e0be8abf9c6a1db6cfdcb31193bbc90e4c08f7
Dillon Parker <prkr93@gmail.com>
2021-12-07 14:08:38 -0700

    Refactor Vampire methods

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -4,13 +4,13 @@ class Vampire {
     this.pet = pet;
     this.thirsty = true;
     this.ouncesDrank = 0;
-    this.drink = function(){
-      if(this.ouncesDrank >= 50){
-        return "I'm too full to drink anymore!";
-      }
-      this.ouncesDrank += 10;
-      this.thirsty = false;
+  }
+  drink () {
+    if(this.ouncesDrank >= 50){
+      return "I'm too full to drink anymore!";
     }
+    this.ouncesDrank += 10;
+    this.thirsty = false;
   }
 }
 
