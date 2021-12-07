5055f9f0b14fdd19ac5056f9eb2ff7240dcfa6ce
Dillon Parker <prkr93@gmail.com>
2021-12-06 18:56:32 -0700

    Add Dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,19 @@
+class Dragon {
+  constructor(name, rider, hungry = true, timesEaten = 0) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = hungry;
+    this.timesEaten = timesEaten;
+    this.greet = function(){
+      return `Hi, ${rider}!`;
+    };
+    this.eat = function(){
+      timesEaten += 1;
+      if(timesEaten === 3){
+        this.hungry = false;
+      }
+    }
+  }
+}
 
+module.exports = Dragon;
