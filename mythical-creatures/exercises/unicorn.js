1787e27e6b0b9f63b1fc35b24c9696fc38d62c07
Dillon Parker <prkr93@gmail.com>
2021-12-06 18:55:22 -0700

    Add Unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,14 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = "white") {
     this.name = name;
     this.color = color;
+    this.isWhite = function(){
+      if(this.color === "white"){ return true; }
+      else{ return false; }
+    }
+    this.says = function(string){
+      return `**;* ${string} *;**`;
+    };
   }
 }
 
