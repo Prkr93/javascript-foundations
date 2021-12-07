edc35b96f47ff88b82900cf7c294c2e8d8dec2a4
Dillon Parker <prkr93@gmail.com>
2021-12-07 14:06:45 -0700

    Refactor Dragon methods

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,17 +1,17 @@
 class Dragon {
-  constructor(name, rider, hungry = true, timesEaten = 0) {
+  constructor(name, rider, hungry = true) {
     this.name = name;
     this.rider = rider;
     this.hungry = hungry;
-    this.timesEaten = timesEaten;
-    this.greet = function(){
-      return `Hi, ${rider}!`;
-    };
-    this.eat = function(){
-      timesEaten += 1;
-      if(timesEaten === 3){
-        this.hungry = false;
-      }
+    this.timesEaten = 0;
+  }
+  greet(){
+    return `Hi, ${this.rider}!`;
+  }
+  eat(){
+    this.timesEaten += 1;
+    if(this.timesEaten === 3){
+      this.hungry = false;
     }
   }
 }
