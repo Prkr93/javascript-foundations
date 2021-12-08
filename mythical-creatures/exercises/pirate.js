0411d35cabae1b46fb562bf330a94f44796d8c76
Dillon Parker <prkr93@gmail.com>
2021-12-08 14:49:38 -0700

    Refactor Pirate methods

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -5,28 +5,28 @@ class Pirate {
     this.cursed = false;
     this.booty = 0;
     this.shipsRobbed = 0;
-    this.robShip = function(){
-      if(this.shipsRobbed >= 5){
-        this.shipsRobbed += 1;
-        this.cursed = true;
-        return "ARG! I've been cursed!";
-      }
-      this.booty += 100;
+  }
+  liftCurse () {
+    if(this.booty >= 300 && this.cursed === true){
+      this.booty -= 300;
+      this.cursed = false;
+      return "Your curse has been lifted!";
+    }
+    else if(this.cursed === false){
+      return "You don't need to lift a curse!";
+    } else{
+      return "You don't have enough booty! Go get a real job!";
+    }
+  }
+  robShip () {
+    if(this.shipsRobbed >= 5){
       this.shipsRobbed += 1;
-      return "YAARRR!";
+      this.cursed = true;
+      return "ARG! I've been cursed!";
     }
-    this.liftCurse = function(){
-      if(this.booty >= 300 && this.cursed === true){
-        this.booty -= 300;
-        this.cursed = false;
-        return "Your curse has been lifted!";
-      }
-      else if(this.cursed === false){
-        return "You don't need to lift a curse!";
-      } else{
-        return "You don't have enough booty! Go get a real job!";
-      }
-    }
+    this.booty += 100;
+    this.shipsRobbed += 1;
+    return "YAARRR!";
   }
 }
 
